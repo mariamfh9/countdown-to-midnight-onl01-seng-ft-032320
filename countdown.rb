7e19a1da0124c57f01da03993e463c94b49ca38d
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-24 19:39:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,13 @@ def countdown(number)
 end 
 
 def countdown_with_sleep(num)
-  
+  while num > 0
+  puts "#{num} SECOND(S)!"
+  num -= 1
+  def sleep(seconds)
+  sleep(5)
+end 
+  end
+  return "HAPPY NEW YEAR!"
   
 end 
